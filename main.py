f826cd4136b7068a4609eb7136cc0a59a7f47b1e
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2025-02-06 14:09:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+item1 = "Phone"
+item1_price = 100000
+item1_quantity = 5
+item1_prcie_total = item1_price * item1_quantity
+
+"""These datatypes are instances of strings or integers."""
+
+print(type(item1)) #str
+print(type(item1_price)) #int
+print(type(item1_quantity)) #int
+print(type(item1_prcie_total)) #int
+
+
+
+
+
+
